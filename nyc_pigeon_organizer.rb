81249efdc1dc8012a305d8b3a9bf8e7f8b44ce6c
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-27 15:19:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   data.each_with_object({}) do |(key, val), fin_arr|
-    value.each do 
+    value.each do |inner_key, name_arr|
+      
+      
+    end 
     
     
     fin_arr
