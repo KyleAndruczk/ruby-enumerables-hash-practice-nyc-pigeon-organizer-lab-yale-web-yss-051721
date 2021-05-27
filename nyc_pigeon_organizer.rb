afe0dfc3ea28288edd0df84c59f08c5b2c0552a4
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-27 15:30:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
           fin_arr[name] = {}
         end
         if !fin_arr[name][key]
-          !fin_arr[name][key] = []
+          fin_arr[name][key] = []
         end
         fin_arr[name][key] << inner_key.to_s
         
