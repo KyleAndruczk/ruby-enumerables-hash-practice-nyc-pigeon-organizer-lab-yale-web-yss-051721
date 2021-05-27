035c9421c83bed0593caf4f06b224d84dacb59ad
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-27 15:25:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  data.each_with_object({}) do |(key, val), fin_arr|
+  final_hash = data.each_with_object({}) do |(key, val), fin_arr|
     value.each do |inner_key, name_arr|
       name_arr.each do |name|
         binding.pry
